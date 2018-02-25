3c6fd857155b13da42c6d7d15350d25971ef7f7a
Dave McKay <dave@polytiersoftware.com>
2018-02-25 15:54:03 -0500

    change to rest api access from collection

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -1,20 +1,10 @@
       angular.module('onepage',[]);
-      angular.module('onepage').controller('BaseCtrl', ['$scope', function ($scope) {
-        $scope.registrations = [{
-          id: 123456555,
-          name: 'Dave McKay',
-          address1: ' 123 Main St',
-          address2: '',
-          city: 'Toronto',
-          prov: 'Ontario',
-          postal: 'M5R 3E8',
-          country: 'Canada',
-          phone: '647-504-7873',
-          ethaccount: 'A64E8870',
-          email: 'dave@promulgareconsulting.com',
-          pictureid: 'A7E00',
-          investknowldge: 'yes'
+      angular.module('onepage').controller('BaseCtrl', ['$scope', '$http', function ($scope, $http) {
 
-        }];
+      	$http.get('/registration').then(function(response){
+      		scope.registrations=response.data;
+
+      	});
+
 
       }]);
