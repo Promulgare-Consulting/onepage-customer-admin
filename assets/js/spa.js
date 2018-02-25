dc7fcb68c8962983ba8bcb8263ae0ee846edd6ba
Dave McKay <dave@polytiersoftware.com>
2018-02-25 15:20:10 -0500

    Start to make registration model integration

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -1,8 +1,20 @@
       angular.module('onepage',[]);
       angular.module('onepage').controller('BaseCtrl', ['$scope', function ($scope) {
-        $scope.things = [{
+        $scope.registration = [{
           id: 123456,
-          name: 'foo'
+          name: 'Dave McKay',
+          address1: ' 123 Main St',
+          address2: '',
+          city: 'Toronto',
+          prov: 'Ontario',
+          postal: 'M5R 3E8',
+          country: 'Canada',
+          phone: '647-504-7873',
+          ethaccount: 'A64E8870',
+          email: 'dave@promulgareconsulting.com',
+          pictureid: 'A7E',
+          investknowldge: 'yes'
+
         }];
 
       }]);
